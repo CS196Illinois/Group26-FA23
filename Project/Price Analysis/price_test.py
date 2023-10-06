1486a103754ecac5812f3db72c4990caab3eeae4
realMattyG <146767396+realMattyG@users.noreply.github.com>
2023-10-06 15:59:16 -0500

    Update price_test.py
    include Wilder smoothing, average true range, and average directional index

--- Project/Price Analysis/price_test.py
+++ Project/Price Analysis/price_test.py
@@ -80,6 +80,109 @@ ax1.legend(['SMA_Ratio'],ncol=3, loc = 'upper left', fontsize = 12)
 ax1.set_facecolor('silver')
 plt.subplots_adjust(left=.09, bottom=.09, right=1, top=.95, wspace=.20, hspace=0)
 plt.show()
+
+
+#Wilder's smoothing
+def Wilder(data, periods):
+    start = np.where(~np.isnan(data))[0][0] #Check if nans present in beginning
+    wilder = np.array([np.nan]*len(data))
+    wilder[start+periods-1] = data[start:(start+periods)].mean() #Simple Moving Average
+    for i in range(start+periods,len(data)):
+        wilder[i] = (wilder[i-1]*(periods-1) + data[i])/periods #Wilder Smoothing
+    return(wilder)
+
+#"Average True Range" Technical Indicator
+all_data['prev_close'] = all_data.groupby('symbol')['Close'].shift(1)
+all_data['TR'] = np.maximum((all_data['High'] - all_data['Low']), 
+                     np.maximum(abs(all_data['High'] - all_data['prev_close']), 
+                     abs(all_data['prev_close'] - all_data['Low'])))
+for i in all_data['symbol'].unique():
+    TR_data = all_data[all_data.symbol == i].copy()
+    all_data.loc[all_data.symbol==i,'ATR_5'] = Wilder(TR_data['TR'], 5)
+    all_data.loc[all_data.symbol==i,'ATR_15'] = Wilder(TR_data['TR'], 15)
+
+all_data['ATR_Ratio'] = all_data['ATR_5'] / all_data['ATR_15']
+
+
+#why doesn't this print??????????
+print(all_data['TR'])
+
+#"Average Directional Index (ADX)" Technical Indicator
+all_data['prev_high'] = all_data.groupby('symbol')['High'].shift(1)
+all_data['prev_low'] = all_data.groupby('symbol')['Low'].shift(1)
+
+all_data['+DM'] = np.where(~np.isnan(all_data.prev_high),
+                           np.where((all_data['High'] > all_data['prev_high']) & 
+         (((all_data['High'] - all_data['prev_high']) > (all_data['prev_low'] - all_data['Low']))), 
+                                                                  all_data['High'] - all_data['prev_high'], 
+                                                                  0),np.nan)
+
+all_data['-DM'] = np.where(~np.isnan(all_data.prev_low),
+                           np.where((all_data['prev_low'] > all_data['Low']) & 
+         (((all_data['prev_low'] - all_data['Low']) > (all_data['High'] - all_data['prev_high']))), 
+                                    all_data['prev_low'] - all_data['Low'], 
+                                    0),np.nan)
+
+for i in all_data['symbol'].unique():
+    ADX_data = all_data[all_data.symbol == i].copy()
+    all_data.loc[all_data.symbol==i,'+DM_5'] = Wilder(ADX_data['+DM'], 5)
+    all_data.loc[all_data.symbol==i,'-DM_5'] = Wilder(ADX_data['-DM'], 5)
+    all_data.loc[all_data.symbol==i,'+DM_15'] = Wilder(ADX_data['+DM'], 15)
+    all_data.loc[all_data.symbol==i,'-DM_15'] = Wilder(ADX_data['-DM'], 15)
+
+all_data['+DI_5'] = (all_data['+DM_5']/all_data['ATR_5'])*100
+all_data['-DI_5'] = (all_data['-DM_5']/all_data['ATR_5'])*100
+all_data['+DI_15'] = (all_data['+DM_15']/all_data['ATR_15'])*100
+all_data['-DI_15'] = (all_data['-DM_15']/all_data['ATR_15'])*100
+
+all_data['DX_5'] = (np.round(abs(all_data['+DI_5'] - all_data['-DI_5'])/(all_data['+DI_5'] + all_data['-DI_5']) * 100))
+
+all_data['DX_15'] = (np.round(abs(all_data['+DI_15'] - all_data['-DI_15'])/(all_data['+DI_15'] + all_data['-DI_15']) * 100))
+
+for i in all_data['symbol'].unique():
+    ADX_data = all_data[all_data.symbol == i].copy()
+    all_data.loc[all_data.symbol==i,'ADX_5'] = Wilder(ADX_data['DX_5'], 5)
+    all_data.loc[all_data.symbol==i,'ADX_15'] = Wilder(ADX_data['DX_15'], 15)
+
+'''
+sns.set()
+
+
+
+#Obtain list of S&100 companies from wikipedia
+resp = requests.get("https://en.wikipedia.org/wiki/S%26P_100")
+convert_soup = bs.BeautifulSoup(resp.text, 'lxml')
+table = convert_soup.find('table',{'class':'wikitable sortable'})
+
+tickers = []
+
+for rows in table.findAll('tr')[1:]:
+    ticker = rows.findAll('td')[0].text.strip()
+    tickers.append(ticker)
+
+all_data = pd.DataFrame()
+test_data = pd.DataFrame()
+no_data = []
+
+#Extract data from Yahoo Finance
+for i in tickers:
+    try:
+        print(i)
+        test_data = pdr.get_data_yahoo(i, start = dt.datetime(1990,1,1), end = dt.date.today())
+        test_data['symbol'] = i
+        all_data = all_data.append(test_data)
+        clear_output(wait = True)
+    except:
+        no_data.append(i)
+
+    clear_output(wait = True)
+
+
+Target_variables = ['SMA_ratio','ATR_5','ATR_15','ATR_Ratio',
+                       'ADX_5','ADX_15','SMA_Volume_Ratio','Stochastic_5','Stochastic_15','Stochastic_Ratio',
+                      'RSI_5','RSI_15','RSI_ratio','MACD']
+for variable in Target_variables:
+    all_data.loc[:,variable] = mstats.winsorize(all_data.loc[:,variable], limits = [0.1,0.1])
 '''
 sns.set()
 
